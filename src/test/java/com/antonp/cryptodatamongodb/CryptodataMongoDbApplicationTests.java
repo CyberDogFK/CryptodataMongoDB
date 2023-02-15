71fe29fc8c2a6b48f76eaa1fbe6566496d4b98ec
AntonPav <streetreadger@gmail.com>
2023-02-15 17:28:20 +0200

    init commit

---
+++ src/test/java/com/antonp/cryptodatamongodb/CryptodataMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antonp.cryptodatamongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptodataMongoDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
